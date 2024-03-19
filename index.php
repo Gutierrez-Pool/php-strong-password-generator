7450857e0bd3f182cf72154aee373d81af1377fd
pool.gutierrez <pool.gutierrezv@gmail.com>
2024-03-19 10:00:12 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Password Generator</title>
+</head>
+<body>
+    <h1>Password Generator</h1>
+    <form action="" method="get">
+        <label for="lunghezza">Lunghezza password:</label>
+        <input type="number" name="lunghezza" min="1" max="10">
+        <button type="submit">Genera</button>
+    </form>
+
+
+</body>
+</html>
